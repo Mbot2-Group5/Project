b52f26f7d3e9f996093a7ef61f078ab318d697a1
StefanRautner <stefan.rautner06@gmail.com>
2024-06-03 08:51:57 +0200

    update multithreading & INFO

--- TEST MULTITHREADING/MBOT-TEST.py
+++ TEST MULTITHREADING/MBOT-TEST.py
@@ -1,8 +1,9 @@
 # Autor: Stefan Rautner
 # imports
+import _thread
 import json
 import time
-import _thread
+
 import cyberpi
 import network
 import usocket
@@ -13,8 +14,8 @@ class MBotController:
     def __init__(self):
         self.UDP_socket = usocket.socket(usocket.AF_INET, usocket.SOCK_DGRAM)
         self.TCP_socket = usocket.socket(usocket.AF_INET, usocket.SOCK_STREAM)
+        self.suicidePreventionThread = _thread.start_new_thread(self.suicidePreventionCheck, ())
         self.suicidePrevention = False
-        self.suicidePreventionThread = None
 
     # "Main" der Klasse
     def start(self):
@@ -79,9 +80,6 @@ class MBotController:
                 cyberpi.led.on(255, 0, 0)
                 return None
 
-            # Neuen Thread für SuicidePrevention
-            self.suicidePreventionThread = _thread.start_new_thread(self.suicidePreventionCheck, ())
-
             # Broadcast senden-Funktion aufrufen
             self.handle_connections()
         except Exception as exception:
@@ -93,7 +91,7 @@ class MBotController:
     def suicidePreventionCheck(self):
         while True:
             try:
-                if cyberpi.ultrasonic2.get(index=1) < 10 and self.suicidePrevention:
+                if cyberpi.ultrasonic2.get(index=1) <= 10 and self.suicidePrevention:
                     cyberpi.mbot2.turn(180)
             except Exception as exception:
                 cyberpi.console.clear()
@@ -188,16 +186,11 @@ class MBotController:
                 # Farben der Heckleuchte setzen
                 if left == 0 and right == 0:
                     # Ambientebeleuchtung
-                    cyberpi.led.on(int("0x" + leftLED[:2], 16), int("0x" + leftLED[2:4], 16),
-                                   int("0x" + leftLED[4:6], 16), id=1)
-                    cyberpi.led.on(int("0x" + middleLeftLED[:2], 16), int("0x" + middleLeftLED[2:4], 16),
-                                   int("0x" + middleLeftLED[:4], 16), id=2)
-                    cyberpi.led.on(int("0x" + middleLED[:2], 16), int("0x" + middleLED[2:4], 16),
-                                   int("0x" + middleLED[4:6], 16), id=3)
-                    cyberpi.led.on(int("0x" + middleRightLED[:2], 16), int("0x" + middleRightLED[2:4], 16),
-                                   int("0x" + middleRightLED[4:6], 16), id=4)
-                    cyberpi.led.on(int("0x" + rightLED[:2], 16), int("0x" + rightLED[2:4], 16),
-                                   int("0x" + rightLED[4:6], 16), id=5)
+                    cyberpi.led.on(int("0x" + leftLED[:2], 16), int("0x" + leftLED[2:4], 16), int("0x" + leftLED[4:6], 16), id=1)
+                    cyberpi.led.on(int("0x" + middleLeftLED[:2], 16), int("0x" + middleLeftLED[2:4], 16), int("0x" + middleLeftLED[:4], 16), id=2)
+                    cyberpi.led.on(int("0x" + middleLED[:2], 16), int("0x" + middleLED[2:4], 16), int("0x" + middleLED[4:6], 16), id=3)
+                    cyberpi.led.on(int("0x" + middleRightLED[:2], 16), int("0x" + middleRightLED[2:4], 16), int("0x" + middleRightLED[4:6], 16), id=4)
+                    cyberpi.led.on(int("0x" + rightLED[:2], 16), int("0x" + rightLED[2:4], 16), int("0x" + rightLED[4:6], 16), id=5)
                 elif left == right and right > 0:
                     # Vorwärts fahren
                     cyberpi.led.on(255, 0, 0, id=1)
