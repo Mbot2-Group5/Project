201088d9f2962232b25f5af47d02b02187aac350
StefanRautner <stefan.rautner06@gmail.com>
2024-06-04 08:10:34 +0200

    Update Close Connection

--- WebSocketServer/IntermediaryServerForMBotConnection.py
+++ WebSocketServer/IntermediaryServerForMBotConnection.py
@@ -149,11 +149,11 @@ async def sendDataToMBot2FromWebApp(websocket):
             elif message == "Close":
                 tcp_socket.send("Disconnect".encode('utf-8'))
                 tcp_socket.close()
+                await websocket.close()
                 print("Client closed")
                 print("Disconnected from Client & MBot")
                 print("Deleting the Intermediary Server...")
                 time.sleep(1)
-                await websocket.close()
                 await deleteScript()
                 break
             elif message == "searchForMBots":
@@ -173,8 +173,8 @@ async def sendDataToMBot2FromWebApp(websocket):
         print(f"Error while sending message to TCP-Server: {e}")
         tcp_socket.send("Disconnect".encode('utf-8'))
         tcp_socket.close()
-        await deleteScript()
         await websocket.close()
+        await deleteScript()
 
 
 # Main
